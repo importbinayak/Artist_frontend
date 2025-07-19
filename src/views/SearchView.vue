87ebc5e383043f5017090734b30a2ebfd1178c29
Binayak Dhungana <thedhungana478@gmail.com>
2025-07-19 22:51:12 +0545

    component

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -110,41 +110,7 @@
                 </div>
 
                 <div class="main-right col-span-1 space-y-4">
-                    <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                        <h3 class="mb-6 text-xl">People you may know</h3>
-
-                        <div class="space-y-4">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center space-x-2">
-                                    <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                                    
-                                    <p class="text-xs"><strong>Code With Stein</strong></p>
-                                </div>
-
-                                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                            </div>
-
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center space-x-2">
-                                    <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                                    
-                                    <p class="text-xs"><strong>Code With Stein</strong></p>
-                                </div>
-
-                                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                            </div>
-
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center space-x-2">
-                                    <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                                    
-                                    <p class="text-xs"><strong>Code With Stein</strong></p>
-                                </div>
-
-                                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                            </div>
-                        </div>
-                    </div>
+                    <PeopleYouMayKnow/>
 
                     <div class="p-4 bg-white border border-gray-200 rounded-lg">
                         <h3 class="mb-6 text-xl">Trends</h3>
@@ -153,7 +119,7 @@
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center space-x-2">
                                     <p class="text-xs">
-                                        <strong>#codewithstein</strong><br>
+                                        <strong>Binayak</strong><br>
                                         <span class="text-gray-500">180 posts</span>
                                     </p>
                                 </div>
@@ -164,7 +130,7 @@
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center space-x-2">
                                     <p class="text-xs">
-                                        <strong>#codewithstein</strong><br>
+                                        <strong>Binayak</strong><br>
                                         <span class="text-gray-500">180 posts</span>
                                     </p>
                                 </div>
@@ -175,7 +141,7 @@
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center space-x-2">
                                     <p class="text-xs">
-                                        <strong>#codewithstein</strong><br>
+                                        <strong>Binayak</strong><br>
                                         <span class="text-gray-500">180 posts</span>
                                     </p>
                                 </div>
@@ -186,7 +152,7 @@
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center space-x-2">
                                     <p class="text-xs">
-                                        <strong>#codewithstein</strong><br>
+                                        <strong>Binayak</strong><br>
                                         <span class="text-gray-500">180 posts</span>
                                     </p>
                                 </div>
@@ -197,7 +163,7 @@
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center space-x-2">
                                     <p class="text-xs">
-                                        <strong>#codewithstein</strong><br>
+                                        <strong>Binayak</strong><br>
                                         <span class="text-gray-500">180 posts</span>
                                     </p>
                                 </div>
@@ -209,3 +175,14 @@
                 </div>
             </div>
 </template>
+
+<script>
+import PeopleYouMayKnow from '@/components/PeopleYouMayKnow.vue'
+export default{
+    name:'SearchView',
+    components:{
+        PeopleYouMayKnow,
+    }
+}
+
+</script>
